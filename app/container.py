625a2e18ac903b9df3e0700904eb72e9d5dcf172
infanasotku <infanasotku@gmail.com>
2026-07-04 13:09:23 +0700

    Add HTTPGdeBenzClient and update container for dependency injection

--- app/container.py
+++ app/container.py
@@ -1,7 +1,10 @@
 from dependency_injector import containers, providers
 
 from app.infra.config import generate_settings
+from app.infra.http.gdebenz import HTTPGdeBenzClient
 
 
 class Container(containers.DeclarativeContainer):
     settings = providers.Singleton(generate_settings)
+
+    gdebenz = providers.Singleton(HTTPGdeBenzClient)
---
+++ app/infra/http/gdebenz.py
@@ -0,0 +1,9 @@
+BASE_URL = "https://gdebenz.ru/api"
+
+
+class HTTPGdeBenzClient:
+    def __init__(self) -> None:
+        pass
+
+    async def get_stations(self):
+        pass
